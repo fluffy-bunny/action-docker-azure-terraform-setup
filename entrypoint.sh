20929f29e9a7ec6b72f976db9392c9a29c49355c
Herb Stahl <ghstahl@gmail.com>
2020-03-24 10:39:13 -0700

    full script

--- entrypoint.sh
+++ entrypoint.sh
@@ -47,18 +47,71 @@ echo 'CONTAINER_NAME:       '$CONTAINER_NAME
 echo 'STORAGE_ACCOUNT_NAME: '$STORAGE_ACCOUNT_NAME
 echo 'KV_NAME:              '$KV_NAME
 
-client_id=`echo $creds | jq --raw-output '.clientId'`
+clientId=`echo $creds | jq --raw-output '.clientId'`
 clientSecret=`echo $creds | jq --raw-output '.clientSecret'`
 tenantId=`echo $creds | jq --raw-output '.tenantId'`
 
-echo 'client_id:             '${clientId:0:4}'*********'
+echo 'clientId:              '${clientId:0:4}'*********'
 echo 'clientSecret:          '${clientSecret:0:4}'*********'
 echo 'tenantId:              '${tenantId:0:4}'*********'
 
-az login --service-principal -u $client_id -p $clientSecret --tenant $tenantId  
+az login --service-principal -u $clientId -p $clientSecret --tenant $tenantId  
 
 az account show 
 
+SUBSCRIPTION_ID="$(az account show --query id -o tsv)"
+echo 'SUBSCRIPTION_ID: '$SUBSCRIPTION_ID
+
+echo "==== Creating Resource Group: $RESOURCE_GROUP_NAME in Location: $LOCATION"
+az group create \
+    --name $RESOURCE_GROUP_NAME \
+    --location $LOCATION
+
+
+echo "====== Creating KEY VAULT:  $KV_NAME ================="
+az keyvault create \
+    --location $LOCATION \
+    --name $KV_NAME \
+    --resource-group $RESOURCE_GROUP_NAME
+    
+# Create storage account
+az storage account create \
+    --resource-group $RESOURCE_GROUP_NAME \
+    --name $STORAGE_ACCOUNT_NAME \
+    --sku Standard_LRS \
+    --encryption-services blob
+
+# Get storage account key
+ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name $STORAGE_ACCOUNT_NAME --query [0].value -o tsv)
+ 
+# Create blob container
+az storage container create \
+    --name $CONTAINER_NAME \
+    --account-name $STORAGE_ACCOUNT_NAME \
+    --account-key $ACCOUNT_KEY
+
+echo "storage_account_name: $STORAGE_ACCOUNT_NAME"
+echo "container_name: $CONTAINER_NAME"
+
+
+SECRET_NAME="terraform-backend-key"
+echo "====== Setting Secret: $SECRET_NAME  in KeyVault:  $KV_NAME ================="
+VALUE=$ACCOUNT_KEY
+az keyvault secret set \
+    -n $SECRET_NAME \
+    --vault-name $KV_NAME \
+    --value "$VALUE" \
+    > /dev/null
+
+echo 'export ARM_ACCESS_KEY=$(az keyvault secret show --name '$SECRET_NAME' --vault-name '$KV_NAME' --query value -o tsv)'
+
+
+echo ::set-output name=storage_account_name::$STORAGE_ACCOUNT_NAME
+echo ::set-output name=container_name::$CONTAINER_NAME
+echo ::set-output name=secret_name::$SECRET_NAME
+echo ::set-output name=keyvault_name::$KV_NAME
+
+
 time=$(date)
 echo ::set-output name=time::$time
- 
+
