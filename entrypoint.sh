ccb5f88dc30e28fc2f66f20014723b1a424ac0a3
Herb Stahl <ghstahl@gmail.com>
2020-03-23 18:39:30 -0700

    testing input

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,41 @@
 #!/bin/sh -l
+die () {
+    echo >&2 "$@"
+    echo "$ ./entrypoint.sh [SHORT_NAME] [LOCATION]"
+    exit 1
+}
+REQUIRED_ARGS=2
+[ "$#" -eq $REQUIRED_ARGS ] || die "$REQUIRED_ARGS argument required, $# provided"
 
-echo "Hello $1"
+echo "Positional Parameters"
+SHORT_NAME=$1
+LOCATION=$2
+
+length=${#SHORT_NAME}
+if [ $length -lt 2 -o $length -gt 11 ] ;then
+    echo "length invalid"
+    die
+fi
+if grep '^[-0-9a-zA-Z]*$' <<<$SHORT_NAME ;
+  then 
+    SHORT_NAME=${SHORT_NAME,,}
+    echo ok;
+  else 
+    echo $SHORT_NAME must be ALPHANUMERIC;
+    die
+fi
+
+RESOURCE_GROUP_NAME="rg-terraform-$SHORT_NAME"
+CONTAINER_NAME="tstate"
+STORAGE_ACCOUNT_NAME="stterraform$SHORT_NAME"
+KV_NAME="kv-tf-$SHORT_NAME"
+
+echo 'SHORT_NAME:           '$SHORT_NAME
+echo 'LOCATION:             '$LOCATION
+echo 'RESOURCE_GROUP_NAME:  '$RESOURCE_GROUP_NAME
+echo 'CONTAINER_NAME:       '$CONTAINER_NAME
+echo 'STORAGE_ACCOUNT_NAME: '$STORAGE_ACCOUNT_NAME
+echo 'KV_NAME:              '$KV_NAME
+
 time=$(date)
 echo ::set-output name=time::$time
