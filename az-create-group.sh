8765c68660e999e0785e56d2b87e3e9a50173877
Herb Stahl <ghstahl@gmail.com>
2020-05-14 07:26:20 -0700

    test script

---
+++ az-create-group.sh
@@ -0,0 +1,25 @@
+#!/bin/sh -l
+die () {
+    echo >&2 "$@"
+    echo "die"
+    exit 1
+}
+# This works
+# az group create --name "rg-terraform-lionking" --location "eastus2" --tags "a=b" "c=a dog"
+
+REQUIRED_ARGS=3
+[ "$#" -eq $REQUIRED_ARGS ] || die "$REQUIRED_ARGS argument required, $# provided"
+ bash --version
+
+resource_group_name=$1  
+location=$2
+tags=$3
+
+echo "resource_group_name:" $resource_group_name
+echo "location:"            $location
+echo "tags"                 $tags
+IFS='***'
+az group create --name "$resource_group_name" --location "$location"  --tags ${tags[*]}
+IFS=' '
+
+
